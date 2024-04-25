c97c0ace68c4aeb55a1f289192faed3ef521188f
Alex Grytsai <a.grytsai1987@gmail.com>
2024-04-25 10:19:02 +0300

    made and connected AWS RDS PostgreSQL data base. created superuser

--- ProjectManagerSystem/settings.py
+++ ProjectManagerSystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from credentials import aws_postgres
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,12 +74,24 @@ WSGI_APPLICATION = "ProjectManagerSystem.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+## Postgres
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": aws_postgres.db_name,
+        "USER": aws_postgres.master_username,
+        "PASSWORD": aws_postgres.password,
+        "HOST": aws_postgres.endpoint,
+        "PORT": aws_postgres.port,
+     }
 }
 
 # Password management in Django
