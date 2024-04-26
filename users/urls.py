5bbba2547c8d6cb40fa41a8b37d78c5ced86c98a
Alex Grytsai <a.grytsai1987@gmail.com>
2024-04-26 11:23:38 +0300

    tested and fixed a bug with LogIn

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.urls import include
+from django.contrib.auth.views import LogoutView
 
-from .views import index_view
+from .views import IndexView
 
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
-    path("", index_view, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
+    path("", IndexView.as_view(), name="index"),
 ]
 
 app_name = "users"
