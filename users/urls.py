3cbc1b782b2b4b3390c85505f799d810aa88dd33
Alex Grytsai <a.grytsai1987@gmail.com>
2024-04-26 11:51:45 +0300

    created ProfileView and page, add path url on this page

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.urls import include
-from django.contrib.auth.views import LogoutView
 
 from .views import IndexView
+from .views import ProfileView
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/logout/", LogoutView.as_view(), name="logout"),
     path("", IndexView.as_view(), name="index"),
+    path("accounts/profile/", ProfileView.as_view(), name="profile"),
 ]
 
 app_name = "users"
