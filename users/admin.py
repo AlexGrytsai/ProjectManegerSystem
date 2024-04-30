ff634b73fab230ad8e240944d17a2fa5b5db62fa
Alex Grytsai <a.grytsai1987@gmail.com>
2024-04-30 09:26:12 +0300

    registered WokerUser, Supervisor and Worker in admin.py. In WorkerUserAdmin add list_display and fieldsets with own fields

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,71 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from .models import WorkerUser
 from .models import Worker
 from .models import Supervisor
 
 
+@admin.register(WorkerUser)
+class WorkerUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "is_active",
+        "title",
+        "position",
+        "phone_number",
+        "telegram",
+        "is_supervisor",
+    )
+
+    details_info = (
+        "Details info", {
+            "fields": (
+                "title",
+                "position",
+                "phone_number",
+                "telegram",
+                "photo",
+            )
+        }
+    )
+
+    permissions = (
+        "Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_supervisor",
+                "is_superuser",
+                "user_permissions",
+            )
+        }
+    )
+
+    fieldsets = (
+        UserAdmin.fieldsets[0],
+        UserAdmin.fieldsets[1],
+        details_info,
+        permissions,
+        UserAdmin.fieldsets[3],
+    )
+
+    # add_fieldsets = UserAdmin.add_fieldsets[0] #+ (
+    #     (
+    #       "Additional info", {
+    #           "fields": (
+    #               "is_active",
+    #               "title",
+    #               "position",
+    #               "phone_number",
+    #               "telegram", "is_supervisor"
+    #           )
+    #       }
+    #     ),
+    # )
+
+
 @admin.register(Supervisor)
-class SupervisorAdmin(UserAdmin):
+class SupervisorAdmin(admin.ModelAdmin):
     pass
 
 
