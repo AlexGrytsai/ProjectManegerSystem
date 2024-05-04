22fba8bb60bbcc319ee22c3b6c4f443181530f03
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-04 13:41:02 +0300

    Created a form for update worker info. Implemented different forms for Supervisor and other workers. Created base a page for update info (need to fix the appearance). The correct address has been entered to redirect to the information update page

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 from users.models import WorkerUser
+from users.models import Role
 
 
 class RegisterForm(UserCreationForm):
@@ -11,3 +13,29 @@ class RegisterForm(UserCreationForm):
             "first_name",
             "last_name",
         )
+
+
+class WorkerUserUpdateForm(forms.ModelForm):
+    class Meta(UserCreationForm.Meta):
+        model = WorkerUser
+
+        fields = ("username",
+                  "email",
+                  "first_name",
+                  "last_name",
+                  "role",
+                  "position",
+                  "is_active",
+                  "phone_number",
+                  "telegram",
+                  "photo",
+                  )
+
+    def __init__(self, *args, **kwargs):
+        user_role = kwargs.pop("user_role")
+        super().__init__(*args, **kwargs)
+
+        if user_role == Role.ENGINEER_MANAGER:
+            self.fields.pop("position")
+            self.fields.pop("role")
+            self.fields.pop("is_active")
