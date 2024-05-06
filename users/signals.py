04850a3adf2ef83f2b653448da2ac4ad6a3e6ebb
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-06 10:39:09 +0300

    Made a function for creating and adding permissions to Groups. Fixed Signal - checks whether such groups exist, if not, creates them. After this, it checks that these groups have all the necessary permissions (if necessary, adds new ones). When you create a new user, he is automatically added to the desired group.

--- users/signals.py
+++ users/signals.py
@@ -1,54 +1,139 @@
 from django.db.models.signals import pre_migrate
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+def create_and_add_permissions_to_groups(group: "Group", **kwargs) -> None:
+    from django.contrib.contenttypes.models import ContentType
+    from django.contrib.auth.models import Permission
+    from .models import WorkerUser
+
+    content_type_user = ContentType.objects.get_for_model(WorkerUser)
+
+    permission_users_app_guest_group = [
+        {
+            "codename": "users.change_user",
+            "name": "Can only change own account information",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.view_user",
+            "name": "View user",
+            "content_type": content_type_user
+        },
+    ]
+
+    permission_users_app_engineer_manager_group = [
+        {
+            "codename": "users.change_user",
+            "name": "Can only change own account information",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.view_user",
+            "name": "View user",
+            "content_type": content_type_user
+        },
+    ]
+
+    permission_users_app_supervisor_group = [
+        {
+            "codename": "users.add_user",
+            "name": "Add new worker",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.change_user",
+            "name": "Can only change own account information",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.change_another_user",
+            "name": "Can change another worker's account",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.activate_user",
+            "name": "Activate worker account",
+            "content_type": content_type_user
+        },
+        {
+            "codename": "users.view_user",
+            "name": "View user",
+            "content_type": content_type_user
+        }
+    ]
+
+    group_name = group.name
+    group_permissions_list = group.permissions.values_list(
+        "codename", flat=True
+    )
+
+    if group_name == "Guest":
+        for permission in permission_users_app_guest_group:
+            if permission["codename"] not in group_permissions_list:
+                new_permission, created = Permission.objects.get_or_create(
+                    **permission
+                )
+                group.permissions.add(new_permission)
+
+    if group_name == "Engineer/Manager":
+        for permission in permission_users_app_engineer_manager_group:
+            if permission["codename"] not in group_permissions_list:
+                new_permission, created = Permission.objects.get_or_create(
+                    **permission
+                )
+                group.permissions.add(new_permission)
+
+    if group_name == "Supervisor":
+        for permission in permission_users_app_supervisor_group:
+            if permission["codename"] not in group_permissions_list:
+                new_permission, created = Permission.objects.get_or_create(
+                    **permission
+                )
+                group.permissions.add(new_permission)
+
+
 @receiver(pre_migrate)
-def create_groups_and_permissions(sender, **kwargs):
-    from django.contrib.auth.models import Group, Permission
+def create_groups_and_permissions(sender, **kwargs) -> None:
+    from django.contrib.auth.models import Group
     from .models import Role
+
     for role in Role:
         group, created = Group.objects.get_or_create(name=role.label)
+
         if created:
-            add_user = Permission.objects.get(codename="users.add_user")
-            change_user = Permission.objects.get(codename="users.change_user")
-            activate_user = Permission.objects.get(
-                codename="users.activate_user"
-            )
-            view_user = Permission.objects.get(codename="users.view_user")
+            group.save()
 
-            if role == Role.GUEST:
-                guest_permissions = [view_user, ]
-                group.permissions.add(*guest_permissions)
-            elif role == Role.ENGINEER_MANAGER:
-                engineer_manager_permissions = [
-                    view_user,
-                ]
-                group.permissions.add(*engineer_manager_permissions)
-            elif role == Role.SUPERVISOR:
-                supervisor_permissions = [
-                    view_user, add_user, change_user, activate_user,
-                ]
-                group.permissions.add(*supervisor_permissions)
+    groups = Group.objects.all()
+
+    for group in groups:
+        create_and_add_permissions_to_groups(group)
 
 
-@receiver(pre_save, sender="users.WorkerUser")
-def update_user_in_group(sender, instance, **kwargs):
-    from .models import WorkerUser
+@receiver(post_save, sender="users.WorkerUser")
+def update_user_in_group(sender, instance, **kwargs) -> None:
+    from django.contrib.auth.models import Group
+
     try:
-        old_role = WorkerUser.objects.get(pk=instance.pk).role
-    except WorkerUser.DoesNotExist:
+        old_role = sender.objects.get(pk=instance.pk).role
+    except sender.DoesNotExist:
         old_role = None
 
+    try:
+        old_group = Group.objects.get(name=old_role)
+    except Group.DoesNotExist:
+        old_group = None
+
     new_role = instance.role
+    new_group = Group.objects.get(name=new_role)
 
     if old_role != new_role:
-        from django.contrib.auth.models import Group
-        old_group = Group.objects.get(name=old_role)
-        new_group = Group.objects.get(name=new_role)
 
         if old_group and old_group in instance.groups.all():
             instance.groups.remove(old_group)
 
         if new_group:
             instance.groups.add(new_group)
+    else:
+        instance.groups.add(new_group)
