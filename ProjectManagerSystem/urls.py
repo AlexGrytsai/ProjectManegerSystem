65857940848acb715bb5054a8778fb71583ac47d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-08 10:47:29 +0300

    Started a new app - projects

--- ProjectManagerSystem/urls.py
+++ ProjectManagerSystem/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("social-auth/", include("social_django.urls", namespace="social")),
     path("", include("users.urls", namespace="users")),
+    path("", include("projects.urls", namespace="projects")),
 ]
