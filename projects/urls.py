879d2abdcc6705e667e2a1ca444271027e0ec936
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-11 10:11:49 +0300

    added a path to view responsible workers for the project. Added to context info:tasks_in_progress_count+tasks_done_count+responsible_workers_count. Added queryset with prefetch_related

--- projects/urls.py
+++ projects/urls.py
@@ -3,12 +3,18 @@ from django.urls import path
 
 from .views import ProjectCreateView
 from .views import ProjectListView
+from users.views import WorkerListView
 
 urlpatterns = [
     path("projects/create/",
          ProjectCreateView.as_view(),
          name="create-project"),
     path("projects/list", ProjectListView.as_view(), name="project-list"),
+    path(
+        "projects/<int:project_id>/workers/",
+        WorkerListView.as_view(),
+        name="project-workers"
+    ),
 ]
 
 app_name = "projects"
