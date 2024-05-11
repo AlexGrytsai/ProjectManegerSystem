e6bd38a273263ac976fa7eee1640fe0ea0f9dba6
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-11 13:24:08 +0300

    Made ProjectUpdateView. For ProjectListView fixed get_context_data - added annotate_params (reduced the number of queries to the database by 2 times)

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db.models.functions import Concat
 from django.urls import reverse_lazy
 from view_breadcrumbs import BaseBreadcrumbMixin
 from django.views.generic import CreateView
@@ -7,9 +8,12 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import TemplateView
 from django.views.generic import UpdateView
+from django.db.models import Count, F, Value
+from django.db.models import Q
 
 from .models import Project
 from .forms import ProjectCreateForm
+from .forms import ProjectUpdateForm
 
 
 class ProjectCreateView(
@@ -60,14 +64,37 @@ class ProjectListView(LoginRequiredMixin, BaseBreadcrumbMixin, ListView):
         total_projects = Project.objects.all().count()
         context["total_workers"] = total_projects
 
-        for project in context["project_list"]:
-            project.tasks_in_progress_count = project.tasks.exclude(
-                status="Done"
-            ).count
-            project.tasks_done_count = project.tasks.filter(
-                status="Done"
-            ).count
-            project.responsible_workers_count = (
-                project.responsible_workers.count()
-            )
+        annotate_params = Project.objects.annotate(
+            tasks_in_progress_count=Count(
+                "tasks", filter=~Q(tasks__status="In Progress")
+            ),
+            tasks_done_count=Count("tasks", filter=Q(tasks__status="Done")),
+            responsible_workers_count=Count("responsible_workers"),
+        )
+
+        context["project_list"] = annotate_params
+
         return context
+
+
+class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Project
+    form_class = ProjectUpdateForm
+    template_name = "projects/project_form.html"
+
+    def test_func(self):
+        return self.request.user.role == "Supervisor"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectUpdateView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
+        context["is_update"] = True
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        return reverse_lazy("projects:project-list")
