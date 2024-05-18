00fcac8df871b8cd0a668b798d0f08fa7b985b64
Alex Grytsai <a.grytsai1987@gmail.com>
2024-05-18 06:24:53 +0300

    Added form_valid in ProjectCreateView - immediately adds Project Lead and adds him to the list of responsible workers. Added distinct for get_annotate_params in ProjectListView. ProjectDeleteView, ProjectDetailView - added get_context_data with "referer". Created ProjectDetailTasksView, TaskDeleteView, TaskDetailView In TaskListView changed queryset- for one request. loads project_tasks and responsible_workers. TaskCreateView - changed form_valid (immediately adds Author and adds him to the list of responsible workers).

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db import transaction
 from django.db.models import Count
 from django.db.models import F
 from django.db.models import Q
@@ -12,10 +13,11 @@ from django.views.generic import ListView
 from django.views.generic import UpdateView
 from view_breadcrumbs import BaseBreadcrumbMixin
 
-from .forms import ProjectCreateForm, TaskUpdateForm
+from .forms import ProjectCreateForm
 from .forms import ProjectUpdateForm
 from .forms import TaskCreateForm
-from .models import Project
+from .forms import TaskUpdateForm
+from .models import Project, TackStatus
 from .models import Task
 from .project_mixins import TaskMixin
 
@@ -39,7 +41,14 @@ class ProjectCreateView(
     def test_func(self) -> bool:
         return self.request.user.role == "Supervisor"
 
-    def get_context_data(self, **kwargs):
+    @transaction.atomic
+    def form_valid(self, form) -> HttpResponseRedirect:
+        form.instance.project_lead = self.request.user
+        result = super(ProjectCreateView, self).form_valid(form)
+        form.instance.responsible_workers.add(self.request.user)
+        return result
+
+    def get_context_data(self, **kwargs) -> dict:
         context = super(ProjectCreateView, self).get_context_data(**kwargs)
         next_url = self.request.GET.get("next")
         if next_url:
@@ -67,10 +76,14 @@ class ProjectListView(LoginRequiredMixin, BaseBreadcrumbMixin, ListView):
     def get_annotate_params() -> dict:
         return Project.objects.annotate(
             tasks_in_progress_count=Count(
-                "tasks", filter=~Q(tasks__status="In Progress")
+                "tasks",
+                filter=~Q(tasks__status=TackStatus.DONE),
+                distinct=True
             ),
             tasks_done_count=Count(
-                "tasks", filter=Q(tasks__status="Done")
+                "tasks",
+                filter=Q(tasks__status=TackStatus.DONE),
+                distinct=True
             ),
             responsible_workers_count=Count("responsible_workers"),
             project_lead_username=F("project_lead__username"),
@@ -80,6 +93,9 @@ class ProjectListView(LoginRequiredMixin, BaseBreadcrumbMixin, ListView):
 
     def get_context_data(self, **kwargs) -> dict:
         context = super(ProjectListView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
         total_projects = Project.objects.count()
         context["total_workers"] = total_projects
 
@@ -121,6 +137,13 @@ class ProjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self) -> bool:
         return self.request.user.role == "Supervisor"
 
+    def get_context_data(self, **kwargs) -> dict:
+        context = super(ProjectDeleteView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
+        return context
+
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
@@ -129,17 +152,46 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
         "responsible_workers", "tasks"
     )
 
+    def get_context_data(self, **kwargs) -> dict:
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
+        return context
+
+
+class ProjectDetailTasksView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/project_task_list.html"
+    queryset = Project.objects.prefetch_related(
+        "tasks",
+        "tasks__responsible_workers"
+    )
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super(ProjectDetailTasksView, self).get_context_data(
+            **kwargs
+        )
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
+        return context
+
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "projects/task_list.html"
     context_object_name = "task_list"
 
+    queryset = Task.objects.prefetch_related(
+        "project_tasks", "responsible_workers"
+    )
+
     def get_context_data(self, **kwargs) -> dict:
         context = super(TaskListView, self).get_context_data(**kwargs)
-
-        project = Project.objects.get(id=self.kwargs.get("project_id"))
-        context["project"] = project
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
 
         return context
 
@@ -160,12 +212,15 @@ class TaskCreateView(
                 self.check_responsible_worker()
         )
 
+    @transaction.atomic
     def form_valid(self, form) -> HttpResponseRedirect:
-        project = self.get_project()
         form.instance.author = self.request.user
-        form.instance.save()
+        result = super(TaskCreateView, self).form_valid(form)
+        form.instance.responsible_workers.add(self.request.user)
+
+        project = self.get_project()
         project.tasks.add(form.instance)
-        return super(TaskCreateView, self).form_valid(form)
+        return result
 
     def get_form_kwargs(self) -> dict[str, dict]:
         kwargs = super(TaskCreateView, self).get_form_kwargs()
@@ -174,9 +229,20 @@ class TaskCreateView(
 
     def get_context_data(self, **kwargs) -> dict:
         context = super(TaskCreateView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
         project = self.get_project()
         context["project"] = project
         return context
+
+    def get_success_url(self) -> str:
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        return reverse_lazy(
+            "projects:task-list", args=[self.kwargs.get("project_id")]
+        )
 
 
 class TaskUpdateView(
@@ -195,8 +261,81 @@ class TaskUpdateView(
                 self.check_responsible_worker()
         )
 
+    def get_form_kwargs(self) -> dict[str, dict]:
+        kwargs = super(TaskUpdateView, self).get_form_kwargs()
+        kwargs["user_role"] = self.request.user.role
+        kwargs["project"] = self.get_project()
+        return kwargs
+
     def get_context_data(self, **kwargs) -> dict:
         context = super(TaskUpdateView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
         project = self.get_project()
         context["project"] = project
+        context["is_update"] = True
+        return context
+
+    def get_success_url(self) -> str:
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        return reverse_lazy(
+            "projects:task-list", args=[self.kwargs.get("project_id")]
+        )
+
+
+class TaskDeleteView(
+    LoginRequiredMixin,
+    TaskMixin,
+    UserPassesTestMixin,
+    DeleteView
+):
+    model = Task
+    template_name = "projects/task_confirm_delete.html"
+    success_url = reverse_lazy("projects:task-list")
+
+    def test_func(self):
+        return (
+                self.request.user.role == "Supervisor"
+        )
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super(TaskDeleteView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
+        return context
+
+    def get_success_url(self) -> str:
+        next_url = self.request.GET.get("next")
+        if next_url:
+            return next_url
+        return reverse_lazy(
+            "projects:task-list"
+        )
+
+
+class TaskDetailView(
+    LoginRequiredMixin,
+    TaskMixin,
+    UserPassesTestMixin,
+    DetailView
+):
+    model = Task
+    template_name = "projects/task_detail.html"
+    context_object_name = "task"
+
+    def test_func(self):
+        return (
+                self.request.user.role == "Supervisor" or
+                self.check_responsible_worker()
+        )
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super(TaskDetailView, self).get_context_data(**kwargs)
+        next_url = self.request.GET.get("next")
+        if next_url:
+            context["referer"] = next_url
         return context
